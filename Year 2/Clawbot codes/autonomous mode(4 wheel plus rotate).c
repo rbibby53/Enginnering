3ba138cf7022462ff1aeeadea0a990d7bd52e314
rbibby53 <rbibby53@gmail.com>
2018-04-12 14:50:33 -0400

    Update autonomous mode(4 wheel plus rotate).c

--- Year 2/Clawbot codes/autonomous mode(4 wheel plus rotate).c
+++ Year 2/Clawbot codes/autonomous mode(4 wheel plus rotate).c
@@ -24,21 +24,24 @@ void pre_auton()
 
 task autonomous()
 {
-	startMotor(leftMotor, 127);//go fword
-	startMotor(rightMotor, 127);
-	wait(5);//wait 5
-	stopMotor(rightMotor);//stop
-	stopMotor(leftMotor);
-	startMotor(leftMotor, 127);//turn right
-	startMotor(rightMotor, -127);
-	wait(2);//wait 2
-	stopMotor(rightMotor);//stop
-	stopMotor(leftMotor);
-	wait(1);//wait 1
-	startMotor(leftMotor, 127);//go fword
-	startMotor(rightMotor, 127);
-	stopMotor(rightMotor);//stop
-	stopMotor(leftMotor);
+	while (1 == 1)
+	{
+		startMotor(leftMotor, 127);//go fword
+		startMotor(rightMotor, 127);
+		wait(5);//wait 5
+		stopMotor(rightMotor);//stop
+		stopMotor(leftMotor);
+		startMotor(leftMotor, 127);//turn right
+		startMotor(rightMotor, -127);
+		wait(2);//wait 2
+		stopMotor(rightMotor);//stop
+		stopMotor(leftMotor);
+		wait(1);//wait 1
+		startMotor(leftMotor, 127);//go fword
+		startMotor(rightMotor, 127);
+		stopMotor(rightMotor);//stop
+		stopMotor(leftMotor);
+	}
 }
 
 //User Control
@@ -96,7 +99,7 @@ task usercontrol()
 		{
 			motor[armMotor2] = 10;      	//hold arm
 		}
-		
+
 		// claw
 		if(vexRT[Btn6U] == 1)       	//If 6U is pressed
 		{
